ff9d37d114f9d743b5722271992ebd5b8d729273
MadhuHS <madhuhs943@gmail.com>
2016-04-17 10:27:05 +0530

    codes till json
    17-4-2016

---
+++ ActivitesDemo/src/com/jspiders/activitesdemo/MainActivity.java
@@ -0,0 +1,47 @@
+package com.jspiders.activitesdemo;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() 
+    {
+    	// TODO Auto-generated method stub
+    	super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+    	// TODO Auto-generated method stub
+    	super.onResume();
+    }
+    
+    @Override
+    protected void onPause() {
+    	// TODO Auto-generated method stub
+    	super.onPause();
+    }
+    
+    @Override
+    protected void onStop() {
+    	// TODO Auto-generated method stub
+    	super.onStop();
+    }
+    
+    @Override
+    protected void onDestroy() {
+    	// TODO Auto-generated method stub
+    	super.onDestroy();
+    }
+   
+    
+}
